c8161140cad6b515a3ba312dd997095c52fb24fe
webroyit <xytank@gmail.com>
2024-01-25 15:10:49 -0500

    Add Dark Mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Logo from "./Logo";
+import DarkModeToggle from "./DarkModeToggle";
 import { Button } from "./ui/button";
 
 function Header() {
   return (
-    <header>
-        <nav>
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+        <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
             <Logo />
-            <Button>Home</Button>
+
+            <div className="flex-1 flex items-center justify-end space-x-4">
+              <DarkModeToggle />
+            </div>
         </nav>
     </header>
   )
